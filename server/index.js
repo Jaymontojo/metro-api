77b79ce699deda4a575417f8492fabd51e9f39b9
Jay <julesmontojo1@gmail.com>
2022-04-08 10:59:33 +0900

    chore: cleanup unnecessary rest server configs

--- server/index.js
+++ server/index.js
@@ -1,20 +1,14 @@
-const db = require("../db/knex");
-//const { setupExpressServer } = require('./server')
-const PORT = process.env.PORT || 3000;
-//const app = setupExpressServer();
 const { ApolloServer} = require("apollo-server");
+const db = require("../db/knex");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
-
+const PORT = process.env.PORT || 3000;
 const server = new ApolloServer({typeDefs, resolvers});
 
 
 (async () => {
   try{
     await db.migrate.latest();
-    // app.listen(PORT, () => {
-    //   console.log(`App is listening at http://localhost:${PORT}`)
-    // });
     server.listen(PORT, () => {
       console.log(`App is listening at http://localhost:${PORT}/graphql`)
     });

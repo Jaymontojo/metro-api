38d3b0978536438b87f21e1896bfdd458460fbc4
Jay <julesmontojo1@gmail.com>
2022-04-12 07:34:10 +0900

    chore: clean up unneccessary comments

--- db/seeds/stations_seed.js
+++ db/seeds/stations_seed.js
@@ -1,9 +1,4 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
   await knex('stations').del();
   await knex('stations').insert([
     {name_en: 'Tokyo Station', ward_id: 1},
